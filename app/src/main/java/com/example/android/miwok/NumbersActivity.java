58409493619448192995e5a439957fd8915be061
Suyash Dayal <suyashdayal@gmail.com>
2017-10-12 14:50:07 +0530

    Change the parent View from RelativeLayout to LinearLayout and instantiate TextView dynamically

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,44 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    final String TAG = "NumbersActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an ArrayList of type String called words
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Creating a LinearLayout object which is referencing to the LinearLayout XML with id 'root_view'
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
+
+        int index = 0;
+        while(index < 10) {
+            // Creating a TextView dynamically on Java and adding it as a child View to rootView
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
     }
 }
