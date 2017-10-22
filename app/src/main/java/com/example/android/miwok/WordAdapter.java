3acdf2276555c36abcec3de22d7a40b32410b625
Suyash Dayal <suyashdayal@gmail.com>
2017-10-22 13:02:09 +0530

    Complete Version 1 of Miwok

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -16,12 +16,14 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -76,8 +78,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
                     R.layout.list_item, parent, false);
         }
 
-        // Find the LinearLayout in the list_item.xml layout with ID text_container
-        LinearLayout textContainer = (LinearLayout) listItemView.findViewById(R.id.text_container);
+        // Find the RelativeLayout in the list_item.xml layout with ID text_container
+        RelativeLayout textContainer = (RelativeLayout) listItemView.findViewById(R.id.text_container);
         // Set the background color of the textContainer to mColor
         textContainer.setBackgroundResource(mColor);
 
