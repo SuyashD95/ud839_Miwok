4abd5a1d67aaa60a7423509b9914c19ca678d19b
Suyash Dayal <suyashdayal@gmail.com>
2017-10-24 15:34:21 +0530

    Create a MiwokFragmentPagerAdapter class which extends FragmentPagerAdapter

---
+++ app/src/main/java/com/example/android/miwok/MiwokFragmentPageAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class MiwokFragmentPageAdapter extends FragmentPagerAdapter {
+
+    public MiwokFragmentPageAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
+
